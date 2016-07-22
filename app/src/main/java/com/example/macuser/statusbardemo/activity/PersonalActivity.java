02b0029d255f49c9d470797aa1a5ecb69815f2e8
tongdexin <tongdexin@yunhuoer.com>
2016-07-22 17:02:51 +0800

    状态栏控制

--- app/src/main/java/com/example/macuser/statusbardemo/activity/PersonalActivity.java
+++ app/src/main/java/com/example/macuser/statusbardemo/activity/PersonalActivity.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 import android.os.PersistableBundle;
 import android.support.v7.app.AppCompatActivity;
 
+import com.example.macuser.statusbardemo.R;
+
 /**
  * Created by macuser on 16/7/22.
  **/
@@ -12,5 +14,6 @@ public class PersonalActivity extends AppCompatActivity {
     @Override
     public void onCreate(Bundle savedInstanceState, PersistableBundle persistentState) {
         super.onCreate(savedInstanceState, persistentState);
+        setContentView(R.layout.activity_personal);
     }
 }
---
+++ app/src/main/java/com/example/macuser/statusbardemo/view/StatusBarCompat.java
@@ -0,0 +1,78 @@
+package com.example.macuser.statusbardemo.view;
+
+import android.app.Activity;
+import android.content.Context;
+import android.os.Build;
+import android.support.v4.view.ViewCompat;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.FrameLayout;
+
+/**
+ * Created by macuser on 16/7/22.
+ **/
+public class StatusBarCompat {
+
+    /***
+     * 状态栏透明
+     **/
+    public static void initStatus(Activity activity) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT && Build.VERSION.SDK_INT < Build.VERSION_CODES.LOLLIPOP) {
+            Window window = activity.getWindow();
+            ViewGroup mContentView = (ViewGroup) activity.findViewById(Window.ID_ANDROID_CONTENT);
+            View mChildView = mContentView.getChildAt(0);
+            if (mChildView != null) {
+                ViewCompat.setFitsSystemWindows(mChildView, false);
+            }
+            int statusBarHeight = getStatusBarHeight(activity);
+            window.addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+            if (mChildView != null && mChildView.getLayoutParams() != null && mChildView.getLayoutParams().height == statusBarHeight) {
+                mContentView.removeView(mChildView);
+                mChildView = mContentView.getChildAt(0);
+            }
+            if (mChildView != null) {
+                FrameLayout.LayoutParams lp = (FrameLayout.LayoutParams) mChildView.getLayoutParams();
+                if (lp != null && lp.topMargin >= statusBarHeight) {
+                    lp.topMargin -= statusBarHeight;
+                    mChildView.setLayoutParams(lp);
+                }
+            }
+        } else {
+            Window window = activity.getWindow();
+            window.addFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
+        }
+    }
+
+
+    public static void clearStatus(Activity activity) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT && Build.VERSION.SDK_INT < Build.VERSION_CODES.LOLLIPOP) {
+            activity.getWindow().clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+        } else {
+            activity.getWindow().clearFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
+        }
+
+    }
+
+    public static int getStatusBarHeight(Context context) {
+        int result = 0;
+        int resId = context.getResources().getIdentifier("status_bar_height", "dimen", "android");
+        if (resId > 0) {
+            result = context.getResources().getDimensionPixelOffset(resId);
+        }
+        return result;
+    }
+
+    //Get alpha color
+    private static int calculateStatusBarColor(int color, int alpha) {
+        float a = 1 - alpha / 255f;
+        int red = color >> 16 & 0xff;
+        int green = color >> 8 & 0xff;
+        int blue = color & 0xff;
+        red = (int) (red * a + 0.5);
+        green = (int) (green * a + 0.5);
+        blue = (int) (blue * a + 0.5);
+        return 0xff << 24 | red << 16 | green << 8 | blue;
+    }
+}
